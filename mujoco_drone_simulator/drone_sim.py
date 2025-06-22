eaf687c463264db8b9281669cc47355875018c5e
Christian Rauch <Rauch.Christian@gmx.de>
2025-06-22 18:49:45 +0200

    basic simulation loop with 'skydio_x2'

--- mujoco_drone_simulator/drone_sim.py
+++ mujoco_drone_simulator/drone_sim.py
@@ -1,6 +1,18 @@
-def main():
-    print('Hi from mujoco_drone_simulator.')
+#!/usr/bin/env python3
+import mujoco
+import mujoco.viewer
+from robot_descriptions.loaders.mujoco import load_robot_description
 
+
+def main():
+    model_name = "skydio_x2"
+    m = load_robot_description(f"{model_name}_mj_description", variant="scene")
+    d = mujoco.MjData(m)
+
+    with mujoco.viewer.launch_passive(m, d) as viewer:
+        while viewer.is_running():
+            mujoco.mj_step(m, d)
+            viewer.sync()
 
 if __name__ == '__main__':
     main()
